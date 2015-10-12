853b81175b7b1bb6cfcfda02d0fbbbcad3ca8c71
hmcdonough <hmcd311@gmail.com>
2015-10-12 16:11:42 -0700

    first commit

---
+++ advancedCalculator/main.swift
@@ -0,0 +1,40 @@
+//
+//  main.swift
+//  advancedCalculator
+//
+//  Created by Harry McDonough on 10/12/15.
+//  Copyright © 2015 Harrison McDonough. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+//add
+
+//subtract
+
+//multiply
+
+//divide
+
+//Generic(function, int1, int2)
+
+//------------------------
+
+//arrayAdd
+
+//arrayMul
+
+//arrayCount
+
+//arrayAvg
+
+//arrayGeneric
+
+//------------------------
+
+//tupleAdd
+
+//tupleSubtract
+

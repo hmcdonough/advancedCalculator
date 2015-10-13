a91ced8240d8855b56264abb7efaa1f2da35452d
hmcdonough <hmcd311@gmail.com>
2015-10-12 17:11:39 -0700

    added console setup

--- advancedCalculator/main.swift
+++ advancedCalculator/main.swift
@@ -10,7 +10,26 @@ import Foundation
 
 print("Hello, World!")
 
+func input() -> String {
+    let keyboard = NSFileHandle.fileHandleWithStandardInput()
+    let inputData = keyboard.availableData
+    let result = NSString(data: inputData, encoding:NSUTF8StringEncoding) as! String
+    return result.stringByTrimmingCharactersInSet(NSCharacterSet.newlineCharacterSet())
+}
+
+func convert(incoming:String) -> Double {
+    if incoming.rangeOfString("-") != nil {
+        return NSNumberFormatter().numberFromString(incoming)!.doubleValue
+    }
+    return NSNumberFormatter().numberFromString(incoming)!.doubleValue
+}
+
+let firstInput = input()
+var numbers = firstInput.componentsSeparatedByString(" ")
+
+let calc = input()
 //add
+
 
 //subtract
 
